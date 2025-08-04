eb3a4f685cf5b0d5ffca9afc8ab45a7391dfbb9b
Mackson Ncube <randomplayables@proton.me>
2025-08-04 21:29:10 +0000

    remove logs in production

--- vite.config.ts
+++ vite.config.ts
@@ -1,46 +1,29 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     proxy: {
-//       '/api': {
-//         target: 'http://localhost:3000', // Proxy to your randomplayables backend
-//         changeOrigin: true,
-//       }
-//     }
-//   }
-// })
-
-
-
-
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-    allowedHosts: ['.loca.lt'],
-    proxy: {
-      '/api': {
-        target: 'http://172.31.12.157:3000', // Proxy to your randomplayables backend
-        changeOrigin: true,
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    server: {
+      host: '0.0.0.0',
+      allowedHosts: ['.loca.lt'],
+      proxy: {
+        '/api': {
+          target: 'http://172.31.12.157:3000', // Proxy to your randomplayables backend
+          changeOrigin: true,
+        }
       }
     }
+  };
+
+  if (command === 'build') {
+    (config as any).build = {
+      esbuild: {
+        drop: ['console', 'debugger'],
+      },
+    };
   }
+
+  return config;
 })
